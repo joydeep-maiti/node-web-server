854442b90f30e2daec78ba95b5b688b1bd3a5ca5
Joydeep Maiti <joydeep127@gmail.com>
2018-02-07 21:06:09 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerHelper('getCurrentYear', ()=> {
+    return new Date().getFullYear();
+})
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+/*app.get('/', (req, res) => {
+    res.send({
+        name:'joy',
+        age:20
+    });
+});*/
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle : 'Home Page',
+        msg : 'welcome to home page'
+    })
+});
+
+app.get('/about', (req, res)=> {
+    res.render('about.hbs', {
+        pageTitle : 'About Page',
+        msg: 'welcome to about page'
+    });
+});
+
+app.listen(3000, ()=> {
+    console.log('started');
+});
